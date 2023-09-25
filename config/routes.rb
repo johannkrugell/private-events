d12f231b041b02f4ad807be28da1a006d0fc05fa
johannkrugell <johannkrugell@gmail.com>
2023-09-26 06:51:16 +1000

    Add Devise to the project Add Devise to the project, add first name and last name to the user model migration.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Gemfile
    	modified:   Gemfile.lock
    	new file:   app/models/user.rb
    	modified:   app/views/layouts/application.html.erb
    	modified:   config/environments/development.rb
    	new file:   config/initializers/devise.rb
    	new file:   config/locales/devise.en.yml
    	modified:   config/routes.rb
    	new file:   db/migrate/20230925204511_devise_create_users.rb
    	modified:   db/schema.rb
    	new file:   test/fixtures/users.yml
    	new file:   test/models/user_test.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   # root "events#index"
-  root "events#index"
+  root 'events#index'
 
   resources :events, only: [:index, :show, :new, :create]
 end
