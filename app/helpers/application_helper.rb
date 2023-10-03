f7b8fa6a9b3b185ed7b2ed5694c452ef7c0637fb
johannkrugell <johannkrugell@gmail.com>
2023-10-04 07:49:13 +1100

    Styled Event Cards Styled Event Card, add footer and random images to the event cards
    On branch style-event-cards
    Changes to be committed:
    	new file:   app/assets/images/alex-kotliarskyi-QBpZGqEMsKg-unsplash.jpg
    	new file:   app/assets/images/alex-kotliarskyi-QBpZGqEMsKg-unsplash.jpg:Zone.Identifier
    	new file:   app/assets/images/annie-spratt-kr_88BakygA-unsplash.jpg
    	new file:   app/assets/images/annie-spratt-kr_88BakygA-unsplash.jpg:Zone.Identifier
    	new file:   app/assets/images/ian-dooley-hpTH5b6mo2s-unsplash.jpg
    	new file:   app/assets/images/ian-dooley-hpTH5b6mo2s-unsplash.jpg:Zone.Identifier
    	new file:   app/assets/images/lee-blanchflower-1dW1vEJLlCQ-unsplash.jpg
    	new file:   app/assets/images/lee-blanchflower-1dW1vEJLlCQ-unsplash.jpg:Zone.Identifier
    	new file:   app/assets/images/redd-f-wOj5odhDOZ0-unsplash.jpg
    	new file:   app/assets/images/redd-f-wOj5odhDOZ0-unsplash.jpg:Zone.Identifier
    	modified:   app/helpers/application_helper.rb
    	modified:   app/views/events/index.html.erb
    	modified:   app/views/events/new.html.erb
    	modified:   app/views/layouts/application.html.erb
    	new file:   app/views/shared/_footer.html.erb
    	modified:   app/views/shared/_navbar.html.erb
    	modified:   app/views/shared/_session_navbar.html.erb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def random_image_path
+    image_files = Dir.glob('app/assets/images/*.{jpg,png,gif}')
+    image_files.sample
+    File.basename(image_files.sample)
+  end
 end
